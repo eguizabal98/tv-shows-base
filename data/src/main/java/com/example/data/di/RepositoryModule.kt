0f19acac1f0847a440be1e9a0361d27d88faccbd
Elias Eguizabal <elias.eguizabal@gmail.com>
2021-02-08 17:18:53 -0600

    :package: Add new repositories to koin module

--- data/src/main/java/com/example/data/di/RepositoryModule.kt
+++ data/src/main/java/com/example/data/di/RepositoryModule.kt
@@ -1,10 +1,14 @@
 package com.example.data.di
 
 import com.example.data.repository.AuthRepositoryImpl
+import com.example.data.repository.ShowDetailsRepositoryImpl
+import com.example.data.repository.ShowsFavoritesRepositoryImpl
 import com.example.data.repository.ShowsRepositoryImpl
 import com.example.data.util.Connectivity
 import com.example.data.util.ConnectivityImpl
 import com.example.domain.repository.AuthRepository
+import com.example.domain.repository.ShowDetailsRepository
+import com.example.domain.repository.ShowsFavoritesRepository
 import com.example.domain.repository.ShowsRepository
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
@@ -12,5 +16,16 @@ import org.koin.dsl.module
 val repositoryModule = module {
     factory<AuthRepository> { AuthRepositoryImpl(authAPI = get(), connectivity = get()) }
     factory<ShowsRepository<Any>> { ShowsRepositoryImpl(dataBase = get()) }
+    factory<ShowDetailsRepository> {
+        ShowDetailsRepositoryImpl(
+            tvShowDetailsAPI = get()
+        )
+    }
     factory<Connectivity> { ConnectivityImpl(context = androidContext()) }
+    factory<ShowsFavoritesRepository> {
+        ShowsFavoritesRepositoryImpl(
+            favoriteShowAPI = get(),
+            favoriteShowDao = get()
+        )
+    }
 }
