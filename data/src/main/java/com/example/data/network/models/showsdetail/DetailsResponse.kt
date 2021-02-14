27a669865ce8f582f54c062e3ddf618dde44c1cb
Elias Eguizabal <elias.eguizabal@gmail.com>
2021-02-14 13:10:41 -0600

    :package: Add details models to manage network response

---
+++ data/src/main/java/com/example/data/network/models/showsdetail/DetailsResponse.kt
@@ -0,0 +1,20 @@
+package com.example.data.network.models.showsdetail
+
+import com.google.gson.annotations.SerializedName
+
+
+data class CreatorResponse(
+    @SerializedName("id") val creatorId: Int = 0,
+    @SerializedName("name") val name: String? = "",
+    @SerializedName("profile_path") val profileImage: String? = ""
+)
+
+data class EpisodeResponse(
+    @SerializedName("id") val id: Int = 0,
+    @SerializedName("air_date") val airDate: String? = "",
+    @SerializedName("name") val name: String? = "",
+    @SerializedName("still_path") val stillPath: String? = ""
+)
+
+
+
