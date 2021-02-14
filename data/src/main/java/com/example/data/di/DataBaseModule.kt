f65a7890a4ee998b098108f965465965dc86ca08
Elias Eguizabal <elias.eguizabal@gmail.com>
2021-02-14 14:04:32 -0600

    :package: Add new classes to koin modules

---
+++ data/src/main/java/com/example/data/di/DataBaseModule.kt
@@ -0,0 +1,31 @@
+package com.example.data.di
+
+import androidx.room.Room
+import com.example.data.database.DataBase
+import com.example.data.paging.ItemBoundaryCallBack
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import org.koin.android.ext.koin.androidContext
+import org.koin.dsl.module
+
+const val DB_NAME = "TvShowsDb"
+
+val dataBaseModule = module {
+    single {
+        Room.databaseBuilder(androidContext(), DataBase::class.java, DB_NAME)
+            .fallbackToDestructiveMigration()
+            .build()
+    }
+    factory { get<DataBase>().tvShowDao() }
+    factory { get<DataBase>().favoriteShowDao() }
+    factory { get<DataBase>().accountDao() }
+    factory { get<DataBase>().detailsDao() }
+    factory { get<DataBase>().creditsDao() }
+    factory { get<DataBase>().seasonDao() }
+
+    single {
+        ItemBoundaryCallBack(tvShowsAPI = get(), dataBase = get(), scope = get())
+    }
+
+    factory { CoroutineScope(Dispatchers.IO) }
+}
