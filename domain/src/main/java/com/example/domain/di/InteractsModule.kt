cb8db468c7ca5a409adf862827dd94a93455019d
Elias Eguizabal <elias.eguizabal@gmail.com>
2021-02-14 13:02:53 -0600

    :package: Add new use cases to koin module

--- domain/src/main/java/com/example/domain/di/InteractsModule.kt
+++ domain/src/main/java/com/example/domain/di/InteractsModule.kt
@@ -1,28 +1,40 @@
 package com.example.domain.di
 
+import com.example.domain.interactors.account.*
+import com.example.domain.interactors.detail.*
+import com.example.domain.interactors.favorite.*
 import com.example.domain.interactors.login.*
-import com.example.domain.interactors.season.GetSeasonsUseCase
-import com.example.domain.interactors.season.GetSeasonsUseCaseImpl
-import com.example.domain.interactors.tvshowsdetail.*
-import com.example.domain.interactors.tvshowsfavorite.*
-import com.example.domain.interactors.tvshowslist.GetTvShowsUseCase
-import com.example.domain.interactors.tvshowslist.GetTvShowsUseCaseImpl
+import com.example.domain.interactors.season.FetchSeasonsUseCase
+import com.example.domain.interactors.season.FetchSeasonsUseCaseImpl
+import com.example.domain.interactors.season.GetSeasonUseCase
+import com.example.domain.interactors.season.GetSeasonUseCaseImpl
+import com.example.domain.interactors.showlist.*
 import org.koin.dsl.module
 
 val interactsModule = module {
+    //Authentication
     factory<GetAuthTokenUseCase> { GetAuthTokenUseCaseImpl(authRepository = get()) }
     factory<GetSessionIdUseCase> { GetSessionIdUseCaseImpl(authRepository = get()) }
     factory<GetAccountIdUseCase> { GetAccountIdUseCaseImpl(authRepository = get()) }
-
+    factory<LogOutUseCase> { LogOutUseCaseImpl(authRepository = get()) }
+    //ShowList
     factory<GetTvShowsUseCase<Any>> { GetTvShowsUseCaseImpl(showsRepository = get()) }
-    factory<GetShowDetailsLocalUseCase> { GetShowDetailsLocalUseCaseImpl(showDetailsRepository = get()) }
-    factory<GetCastListUseCase> { GetCastListUseCaseImpl(showDetailsRepository = get()) }
-    factory<PutShowFavoriteUseCase> { PutShowFavoriteUseCaseImpl(showsFavoritesRepository = get()) }
-    factory<RefreshShowsFavoritesUseCase> {
-        RefreshShowsFavoritesUseCaseImpl(
-            showsFavoritesRepository = get()
-        )
-    }
-    factory<GetFavoriteShowsUseCase> { GetFavoriteShowsUseCaseImpl(showsFavoritesRepository = get()) }
-    factory<GetSeasonsUseCase> { GetSeasonsUseCaseImpl(seasonsRepository = get()) }
+    factory<ChangeFilterUseCase> { ChangeFilterUseCaseImpl(showsRepository = get()) }
+    factory<SetInitialFilter> { SetInitialFilterImpl(showsRepository = get()) }
+    //Details
+    factory<FetchDetailsUseCase> { FetchDetailsUseCaseImpl(detailsRepository = get()) }
+    factory<FetchCastListUseCase> { FetchCastListUseCaseImpl(detailsRepository = get()) }
+    factory<GetCreditsUseCase<Any>> { GetCreditsUseCaseImpl(detailsRepository = get()) }
+    factory<GetDetailsUseCase<Any>> { GetDetailsUseCaseImpl(detailsRepository = get()) }
+    //FavoriteShows
+    factory<PutFavoriteUseCase> { PutFavoriteUseCaseImpl(favoritesRepository = get()) }
+    factory<RefreshFavoritesUseCase> { RefreshFavoritesUseCaseImpl(favoritesRepository = get()) }
+    factory<GetFavoritesUseCase<Any>> { GetFavoritesUseCaseImpl(favoritesRepository = get()) }
+    //Seasons
+    factory<FetchSeasonsUseCase> { FetchSeasonsUseCaseImpl(seasonsRepository = get()) }
+    factory<GetSeasonUseCase<Any>> { GetSeasonUseCaseImpl(seasonsRepository = get()) }
+    //Account
+    factory<GetAccountUseCase> { GetAccountUseCaseImpl(accountRepository = get()) }
+    factory<FetchAccountUseCase> { FetchAccountUseCaseImpl(accountRepository = get()) }
+
 }
