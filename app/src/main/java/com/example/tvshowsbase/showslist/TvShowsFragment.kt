c444a5847b92738abac50287b5d676fe3fd6d413
Elias Eguizabal <elias.eguizabal@gmail.com>
2021-02-14 14:16:41 -0600

    :package: Change fragment to use livedata responses

--- app/src/main/java/com/example/tvshowsbase/showslist/TvShowsFragment.kt
+++ app/src/main/java/com/example/tvshowsbase/showslist/TvShowsFragment.kt
@@ -1,6 +1,9 @@
 package com.example.tvshowsbase.showslist
 
 import android.content.SharedPreferences
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkRequest
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,9 +12,11 @@ import android.widget.AdapterView
 import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.data.paging.ItemBoundaryCallBack
+import com.example.data.util.Connectivity
 import com.example.domain.model.FilterType
+import com.example.domain.model.InternalErrorCodes
 import com.example.domain.model.TvShow
+import com.example.domain.model.WorkState
 import com.example.tvshowsbase.R
 import com.example.tvshowsbase.databinding.TvShowsFragmentBinding
 import com.example.tvshowsbase.login.LoginFragment.Companion.ACCOUNT_KEY
@@ -20,15 +25,17 @@ import com.google.android.material.snackbar.Snackbar
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
+
 class TvShowsFragment : Fragment(),
     TvShowsAdapter.ItemClickListener {
 
     private val viewModel: TvShowsViewModel by viewModel()
-    private val itemBoundaryCallBack: ItemBoundaryCallBack by inject()
     private val sharedPreferences: SharedPreferences by inject()
+    private val connectivityManager: Connectivity by inject()
 
     private lateinit var binding: TvShowsFragmentBinding
     private var tvShowsAdapter: TvShowsAdapter? = null
+    private var noConnection = false
 
 
     override fun onCreateView(
@@ -44,41 +51,43 @@ class TvShowsFragment : Fragment(),
         tvShowsAdapter = TvShowsAdapter(this)
         binding.tvShowsRecycler.adapter = tvShowsAdapter
 
-        val lastFilterPosition = sharedPreferences.getInt("filter", 0)
-
-        when (lastFilterPosition) {
-            0 -> {
-                itemBoundaryCallBack.setInitialFilter(FilterType.POPULAR)
-            }
-            1 -> {
-                itemBoundaryCallBack.setInitialFilter(FilterType.TOP_RATE)
-            }
-            2 -> {
-                itemBoundaryCallBack.setInitialFilter(FilterType.ON_AIR)
-            }
-            3 -> {
-                itemBoundaryCallBack.setInitialFilter(FilterType.AIRING_TODAY)
-            }
-        }
-
-        binding.filterSpinner.setSelection(lastFilterPosition)
+        setLastFilterPosition()
 
         return binding.root
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setHasOptionsMenu(true)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         createViewModelObservers()
         createBindingObservers()
+        setNetworkStateCallbacks()
         val accountId = sharedPreferences.getInt(ACCOUNT_KEY, 0)
         val sessionId = sharedPreferences.getString(SESSION_KEY, "").toString()
         viewModel.getFavorites(accountId, sessionId, 1)
     }
+
+
+    private fun setLastFilterPosition() {
+        val lastFilterPosition = sharedPreferences.getInt("filter", 0)
+
+        when (lastFilterPosition) {
+            0 -> {
+                viewModel.setInitialFilter(FilterType.POPULAR)
+            }
+            1 -> {
+                viewModel.setInitialFilter(FilterType.TOP_RATE)
+            }
+            2 -> {
+                viewModel.setInitialFilter(FilterType.ON_AIR)
+            }
+            3 -> {
+                viewModel.setInitialFilter(FilterType.AIRING_TODAY)
+            }
+        }
+
+        binding.filterSpinner.setSelection(lastFilterPosition)
+    }
+
 
     private fun createBindingObservers() {
         binding.filterSpinner.onItemSelectedListener =
@@ -89,35 +98,106 @@ class TvShowsFragment : Fragment(),
                     position: Int,
                     id: Long
                 ) {
-                    sharedPreferences.edit { this.putInt("filter", position).apply() }
-                    when (position) {
-                        0 -> {
-                            itemBoundaryCallBack.onChange(FilterType.POPULAR)
-                        }
-                        1 -> {
-                            itemBoundaryCallBack.onChange(FilterType.TOP_RATE)
-                        }
-                        2 -> {
-                            itemBoundaryCallBack.onChange(FilterType.ON_AIR)
-                        }
-                        3 -> {
-                            itemBoundaryCallBack.onChange(FilterType.AIRING_TODAY)
-                        }
-                    }
+                    changeFilter(position)
                 }
 
                 override fun onNothingSelected(parent: AdapterView<*>?) {
                 }
 
             }
+
+        binding.toolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.profile_menu -> {
+                    findNavController().navigate(TvShowsFragmentDirections.actionTvShowsFragmentToProfileFragment())
+                    true
+                }
+                R.id.logOut_menu -> {
+                    val sessionId = sharedPreferences.getString(SESSION_KEY, "")
+                    sessionId?.let {
+                        viewModel.logOut(sessionId)
+                    }
+                    true
+                }
+                else -> true
+            }
+        }
     }
 
     private fun createViewModelObservers() {
         viewModel.showsList.observe(viewLifecycleOwner, {
-            tvShowsAdapter?.submitList(it)
+            it?.let { tvShowsAdapter?.submitList(it) }
+        })
+
+        viewModel.logOutState.observe(viewLifecycleOwner, {
+            handleResponse(response = it, successAction = {
+                sharedPreferences.edit().clear().apply()
+                findNavController().navigate(
+                    TvShowsFragmentDirections.actionTvShowsFragmentToLoginFragment(
+                        true
+                    )
+                )
+            })
         })
     }
 
+    private fun setNetworkStateCallbacks() {
+        val request = NetworkRequest.Builder().build()
+        connectivityManager.getConnectivityManager().registerNetworkCallback(request,
+            object : ConnectivityManager.NetworkCallback() {
+                override fun onLost(network: Network) {
+                    super.onLost(network)
+                    showSnackBar(getString(R.string.no_internet_access))
+                    noConnection = true
+                }
+
+                override fun onAvailable(network: Network) {
+                    super.onAvailable(network)
+                    if (noConnection) {
+                        noConnection = true
+                        if (viewModel.showsList.value?.isEmpty() == true) {
+                            changeFilter(binding.filterSpinner.selectedItemPosition)
+                        }
+                    }
+                }
+            })
+    }
+
+    private fun <T : Any> handleResponse(response: WorkState<T>, successAction: (T) -> Unit) {
+        when (response) {
+            WorkState.Loading -> {
+                startProgress()
+            }
+            is WorkState.Success -> {
+                stopProgress()
+                successAction(response.value)
+            }
+            is WorkState.Failure -> {
+                stopProgress()
+                showErrorMessage(response.error)
+            }
+            else -> {
+            }
+        }
+    }
+
+    private fun changeFilter(position: Int) {
+        sharedPreferences.edit { this.putInt("filter", position).apply() }
+        when (position) {
+            0 -> {
+                viewModel.changeFilter(FilterType.POPULAR)
+            }
+            1 -> {
+                viewModel.changeFilter(FilterType.TOP_RATE)
+            }
+            2 -> {
+                viewModel.changeFilter(FilterType.ON_AIR)
+            }
+            3 -> {
+                viewModel.changeFilter(FilterType.AIRING_TODAY)
+            }
+        }
+    }
 
     override fun onItemClicked(tvShow: TvShow) {
         showSnackBar(tvShow.name)
@@ -126,6 +206,24 @@ class TvShowsFragment : Fragment(),
                 tvShow.showId
             )
         )
+    }
+
+    private fun showErrorMessage(response: InternalErrorCodes) {
+        when (response) {
+            InternalErrorCodes.NO_INTERNET_ACCESS -> showSnackBar(getString(R.string.no_internet_access))
+            InternalErrorCodes.BAD_CREDENTIALS -> showSnackBar(getString(R.string.error_provider))
+            InternalErrorCodes.NOT_FOUND -> showSnackBar(getString(R.string.error_provider))
+            InternalErrorCodes.NOT_SPECIFIC -> showSnackBar(getString(R.string.try_again))
+            InternalErrorCodes.NOT_DB_ENTRY -> showSnackBar(getString(R.string.no_local_available))
+        }
+    }
+
+    private fun startProgress() {
+        binding.listProgressBar.visibility = View.VISIBLE
+    }
+
+    private fun stopProgress() {
+        binding.listProgressBar.visibility = View.GONE
     }
 
     private fun showSnackBar(message: String) {
